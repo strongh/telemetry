0fd1c8b5932acd26a88329b29d2c0ac8b3ba3137
Homer Strong <homer.strong@gmail.com>
2016-09-02 10:18:54 -0700

    add earthtones example app

---
+++ examples/earthtones_example/server.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(maptools)
+library(ggplot2)
+library(earthtones)
+
+shinyServer(function(input, output) {
+  output$coord.earthtones.plot <- renderPlot({
+    lat <- input$telem$lat
+    lon <- input$telem$lon
+    ## see https://github.com/wcornwell/earthtones for more info
+    if (!is.null(lat) && !is.null(lon))
+      # the if check is b/c the browser
+      # seems to initialize lat/lon as null
+      get_earthtones(latitude = lat, longitude=lon,
+                     zoom=as.numeric(input$zoom),
+                     number_of_colors=as.numeric(input$colors))
+  })
+})
---
+++ examples/earthtones_example/ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(telemetry)
+
+
+shinyUI(fluidPage(
+  titlePanel("Telemetry example: coordinates"),
+  sidebarLayout(
+    sidebarPanel(
+      coordinatesInput("telem"), # this prompts the user to share location
+      "This Shiny app is a simple demonstration of using the ",
+      a("telemetry", href="https://github.com/strongh/telemetry"),
+      " package to determine user location. The user location is passed to ",
+      a("earthtones"),
+      " which then determines a color palette from the satellite imagery at
+      the user's location. It usually takes a few seconds for the imagery to appear.",
+      selectInput("zoom", "Zoom level (lower zoom -> larger area)", 2:20, selected=12),
+      selectInput("colors", "Number of colors in palette", 2:20, selected=8)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("coord.earthtones.plot")
+    )
+  )
+))
+
+
